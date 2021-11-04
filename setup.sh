ff0944bb57d62ad20338700dd61d6827ce2c1215
Andrew Barchuk <andrew@raindev.io>
2021-11-04 21:25:53 +0100

    Update setup.sh
    Includes small fixes and removed the need for sudo

--- setup.sh
+++ setup.sh
@@ -2,19 +2,17 @@
 # Downloads github.com/raindev/env and launches setup
 set -euo pipefail
 
-home=/home/raindev
-
 if ! command -v git > /dev/null ; then
 	if command -v xbps-install > /dev/null ; then
-		xbps-install --sync --yes git
+		sudo xbps-install --sync --yes git
 	else
 		echo "Don't know how to install git"
 	fi
 fi
 
-if [ ! -e "$home/code/env" ]; then
-	su raindev -c "git clone https://github.com/raindev/env.git '$home/code/env/'"
-	cd "$home/code/env"
-	su raindev -c "git remote set-url git@github.com:raindev/env.git"
+if [ ! -e "$HOME/code/env" ]; then
+	git clone https://github.com/raindev/env.git "$HOME/code/env/"
+	cd "$HOME/code/env"
+	git remote set-url origin git@github.com:raindev/env.git
 fi
-"$home/code/env/configure"
+"$HOME/code/env/configure"
