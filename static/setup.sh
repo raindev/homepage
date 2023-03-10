d7deb7d6bf3c4a4238980e8a821b009cb767fb43
Andrew Barchuk <andrew@raindev.io>
2023-03-10 14:02:00 +0100

    Update bootstrapping script

--- static/setup.sh
+++ static/setup.sh
@@ -1,11 +1,12 @@
-#!/usr/bin/env bash
-# Downloads github.com/raindev/env and launches setup
-set -euo pipefail
+#!/usr/bin/env sh
+# Downloads raindev/nix-config/configure and launches setup
 
 if ! command -v nix > /dev/null; then
 	echo '>installing Nix'
-	sh <(curl -sSL https://nixos.org/nix/install) --daemon
+	curl -sSL https://nixos.org/nix/install > /tmp/nix-install.sh
+	sh /tmp/nix-install.sh --daemon
+	source /nix/var/nix/profiles/default/etc/profile.d/nix.sh
 fi
-curl -sSL https://raw.githubusercontent.com/raindev/nix-config/main/configure > /tmp/configure
-chmod +x /tmp/configure
-/tmp/configure
+curl -sSL https://raw.githubusercontent.com/raindev/nix-config/main/configure > /tmp/configure.sh
+chmod +x /tmp/configure.sh
+/tmp/configure.sh
